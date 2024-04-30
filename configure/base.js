835ae41838752b9fedfa3c67c0a80cfba0a5367f
deepak90221 <116907628+deepak90221@users.noreply.github.com>
2024-04-30 13:43:20 +0530

    Update base.js

--- configure/base.js
+++ configure/base.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect(process.env.mongodb+srv://admin1:admin1@cluster0.l8awxgl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
